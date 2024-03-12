9c4938c198f8bcd2819837cd4178ecc4f87ab64e
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-03-13 03:16:38 +0600

    updated

--- src/components/main-timer/index.jsx
+++ src/components/main-timer/index.jsx
@@ -6,13 +6,13 @@ const MainTimer = ({ hours, minutes, seconds, convertToBangla, message }) => {
       className={`bottom main-timer text-white w-full p-3 ${hours >= 0 && minutes >= 0 && seconds >= 0 && "gap-2"
         } flex justify-center items-center`}
     >
-      {/* <span className="lh-1 timer-span font-bold text-xl">
+      <span className="lh-1 timer-span font-bold text-xl">
         {hours > 0 && convertToBangla(hours) + " ঘন্টা "}
-      </span> */}
-      <span className="lh-1 timer-span font-bold text-[30px]">
+      </span>
+      <span className="lh-1 timer-span font-bold text-xl">
         {minutes > 0 && convertToBangla(minutes) + " মিনিট "}
       </span>
-      <span className="lh-1 timer-span font-bold text-[30px]">
+      <span className="lh-1 timer-span font-bold text-xl">
         {seconds > 0 && convertToBangla(seconds) + " সেকেন্ড"}
       </span>
 
