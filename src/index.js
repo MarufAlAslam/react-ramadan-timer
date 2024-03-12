2a1838395ef7de58eab398ab2de08ce9424a1c3a
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-03-12 17:07:58 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import "./assets/css/config.css";
 import "./assets/css/globals.css";
 import Timer from "./pages/timer";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("timer-page"));
 root.render(<Timer />);
